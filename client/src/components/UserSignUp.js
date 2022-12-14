ebd3c0e1f7e8c9db74889681b53b997aac2e3656
mariadlcruzz0-0 <mariadlcruzz0.0@gmail.com>
2022-12-13 16:27:04 -0800

    added a cancel and submit function inside my UserSignUp component

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -4,6 +4,46 @@ const UserSignUp = () => {
   return (
     <div>UserSignUp</div>
   )
+  submit = () => {
+    const { context } = this.props;
+
+    const {
+      name,
+      username,
+      password,
+    } = this.state;
+
+    const user = {
+      name,
+      username,
+      password,
+    };
+    context.data.createUser(user)
+      .then( errors => {
+        if (errors.length) {
+          this.setState({ errors });
+        } else {
+          console.log(`${username} is successfully signed up and authenticated!`);
+        }
+      })
+      .catch( err => {
+        console.log(err);
+        //pushes to history stack
+        this.props.history.push('/error');
+      });
+  }
+
+  cancel = () => {
+    this.props.history.push('/');
+  }
+
+
+
+
+
+
+
+
 }
 
 export default UserSignUp
